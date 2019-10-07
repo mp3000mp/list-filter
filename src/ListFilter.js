7ff00c1b33f443dea6e5702dad90231533ab6c95
mp3000mp <moussadedijon@gmail.com>
2019-10-08 01:11:43 +0200

    basic example

--- src/ListFilter.js
+++ src/ListFilter.js
@@ -1,24 +1,73 @@
 'use strict';
 
 /**
+ * Is launched for each list items
+ * @callback onUpdateCallback
+ * @param {string} searchValue
+ * @param {HTMLElement} listItem
+ * @param {boolean} isMatching
+ * @return boolean
+ *
+ * Is launched before finish
+ * @callback onAfterSearchCallback
+ * @param {string} searchValue
+ * @param {HTMLElement[]} foundElements
+ */
+
+/**
  * @constructor
  */
-function Listfilter () {
+function ListFilter () {
 
 	/**
-	 * Finds all tr[data-href] and wrap <td> contents in a <a> tag
-	 * Possibility to set data-target with a valid target attribute value
-	 * @param DOMElement
+	 * @param {HTMLElement} trigger
+	 * @param {HTMLElement} list
+	 * @param {object} options
+	 * @param {boolean} options.caseSensitive
+	 * @param {Number} options.keyupDelay
+	 * @param {boolean} options.searchInHtml
+	 * @param {onUpdateCallback} options.onSearch
+	 * @param {onAfterSearchCallback} options.onAfterSearch
 	 */
-	this.init = (DOMElement) => {
-		const triggers = DOMElement || document.querySelectorAll('input[data-list]');
-		triggers.forEach(function (trigger) {
-			console.log(trigger);
-		});
+	this.init = (trigger, list, options) => {
+		//let me = this;
+		let timeoutOnGoing;
+
+		// options
+		let defaults = {
+			caseSensitive: false,
+			keyupDelay: 200,
+			onAfterSearchCallback: false,
+			onUpdateCallback: false,
+			searchInHtml: false
+		};
+		options = Object.assign(defaults, options);
+
+		// event
+		trigger.onkeyup = function () {
+			clearTimeout(timeoutOnGoing);
+			setTimeout(function () {
+				// todo caseSensitive
+				let regex = new RegExp( `.*${ trigger.value.toLowerCase().trim() }.*`);
+				let listItems = list.getElementsByTagName('li');
+				for(let li of listItems){
+					// todo searchIHtml
+					// todo caseSensitive
+					let match = regex.test(li.innerHTML.toLowerCase());
+					// todo onSearch
+					if(match){
+						li.style.display = '';
+					}else{
+						li.style.display = 'none';
+					}
+				}
+				// todo onAfterSearch
+			}, options.keyupDelay);
+		};
 	};
 }
 
-const oListFilter = new Listfilter();
+const oListFilter = new ListFilter();
 module.exports = oListFilter;
 if (typeof window !== 'undefined') {
 	window.ListFilter = oListFilter;
